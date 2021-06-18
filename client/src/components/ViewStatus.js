0fb47871cec400efd185e14f41b787189e122209
r2r05rishabh <44518549+r2r05rishabh@users.noreply.github.com>
2021-06-18 19:22:00 +0530

    View Status added

--- client/src/components/ViewStatus.js
+++ client/src/components/ViewStatus.js
@@ -1,10 +1,90 @@
-const ViewStatus = () =>
-{
-    return(
-        <>
-        <h1>status not updated yet</h1>
-        </>
-    )
+import React, {useEffect, useState} from 'react';
+
+const ViewStatus = () => {
+
+    const [data, setData] = useState([]);
+
+
+    // GET request function to your Mock API
+
+    const callStatus = () => {
+
+        fetch('/viewStatus', {
+            method: "GET",
+            headers: {
+                Accept: "appllication/json",
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        })
+
+            .then(res => res.json())
+
+            .then(json => setData(json));
+
+    }
+
+
+
+    // Calling the function on component mount
+
+    useEffect(() => {
+
+        callStatus();
+
+    }, []);
+
+
+
+    return (
+
+        <div className="container">
+
+            <h1>View Complaint</h1>
+
+            <table>
+
+                <thead>
+
+                <tr>
+
+                    <th>Student Name</th>
+
+                    <th>Complaint</th>
+
+                    <th>Status</th>
+
+                </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {
+
+                        data.map((data) => (
+
+                            <tr key={data.name}>
+
+                                <td>{data.complaint}</td>
+
+                                <td>{data.complaintStatus}</td>
+
+                                <td/>
+
+                            </tr>
+
+                        ))
+
+                    }
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+    );
 
 }
-export default ViewStatus
+export default ViewStatus 
